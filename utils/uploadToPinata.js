06edbcf8d8c11789f015598abfc13ddb3df96495
rossiytsew <slavarossiytsev@gmail.com>
2023-05-24 23:16:12 +0300

    Adding script for automatic uploading images to Pinata.

---
+++ utils/uploadToPinata.js
@@ -0,0 +1,29 @@
+const pinataSDK = require("@pinata/sdk")
+const path = require("path")
+const fs = require("fs")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataApiSecret = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataApiSecret)
+
+async function storeImages(imagesFilePath) {
+    const fillImagesPath = path.resolve(imagesFilePath)
+    const files = fs.readdirSync(fillImagesPath)
+    let responses = []
+    console.log("Uploading to Pinata...")
+    for (fileIndex in files) {
+        console.log(`Working on ${fileIndex}...`)
+        const readableStreamForFile = fs.createReadStream(`${fillImagesPath}/${files[fileIndex]}`)
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFile)
+            responses.push(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return { responses, files }
+}
+
+module.exports = { storeImages }
