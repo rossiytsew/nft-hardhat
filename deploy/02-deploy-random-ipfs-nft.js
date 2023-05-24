aafe7e3b68177a5d85458f04fc113e5322430e8f
rossiytsew <slavarossiytsev@gmail.com>
2023-05-24 23:16:59 +0300

    Initial creation for random ipfs nft deploy script.

---
+++ deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,48 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const { networkConfig } = require("../helper-hardhat-config")
+const { storeImages } = require("../utils/uploadToPinata")
+
+const imagesLocation = "./images/randomNft"
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    let tokenUris
+
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("---------------------------------------")
+    await storeImages(imagesLocation)
+    // const args = [
+    //     vrfCoordinatorV2Address,
+    //     subscriptionId,
+    //     networkConfig[chainId].gasLane,
+    //     networkConfig[chainId].callbackGasLimit,
+    //     //
+    //     networkConfig[chainId].mintFee
+    // ]
+}
+
+async function handleTokenUris() {
+
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
