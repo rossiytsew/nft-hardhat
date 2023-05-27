e41980646b9873d285aebbdc1d6da3744c566288
rossiytsew <slavarossiytsev@gmail.com>
2023-05-27 22:03:44 +0300

    Adding unit tests for requestNft and fulfillRandomWords functions of random ipfs nft contract.

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -23,4 +23,58 @@ const { developmentChains } = require("../../helper-hardhat-config")
                 assert.equal(isInitialized, true)
             })
         })
+
+        describe("requestNft", () => {
+            it("fails if payment isn't sent with the request", async () => {
+                await expect(randomIpfsNft.requestNft()).to.be.revertedWith(
+                    "RandomIpfsNft__NeedMoreETHSent"
+                )
+            })
+            it("reverts if payment amount is less than the mint fee", async () => {
+                const mintFee = await randomIpfsNft.getMintFee()
+                await expect(randomIpfsNft.requestNft({ 
+                    value: mintFee.sub(ethers.utils.parseEther("0.001")) 
+                })).to.be.revertedWith("RandomIpfsNft__NeedMoreETHSent")
+            })
+            it("emits an event and kicks off a random word request", async () => {
+                const mintFee = await randomIpfsNft.getMintFee()
+                await expect(randomIpfsNft.requestNft({ 
+                    value: mintFee.toString() 
+                })).to.emit(randomIpfsNft, "NftRequested")
+            })
+        })
+
+        describe("fulfillRandomWords", () => {
+            it("mints NFT after random number is returned", async () => {
+                await new Promise(async (resolve, reject) => {
+                    randomIpfsNft.once("NftMinted", async () => {
+                        try {
+                            const tokenUri = await randomIpfsNft.tokenURI("0")
+                            const tokenCounter = await randomIpfsNft.getTokenCounter()
+                            assert(tokenUri.toString().includes("ipfs://"))
+                            assert.equal(tokenCounter.toString(), "1")
+                            resolve()
+                        } catch (error) {
+                            console.log(error)
+                            reject(error)
+                        }
+                    })
+
+                    try {
+                        const mintFee = await randomIpfsNft.getMintFee()
+                        const requestNftResponse = await randomIpfsNft.requestNft({ 
+                            value: mintFee.toString()
+                        })
+                        const requestNftReceipt = await requestNftResponse.wait(1)
+                        await vrfCoordinatorV2Mock.fulfillRandomWords(
+                            requestNftReceipt.events[1].args.requestId,
+                            randomIpfsNft.address
+                        )
+                    } catch (error) {
+                        console.log(error)
+                        reject(error)
+                    }
+                })
+            })
+        })
     })
